50f68cc7dd303a15c50af08a1da21ac25fd9012c
Bino26 <hbino1999@gmail.com>
2024-04-14 23:54:49 +0000

    Add Models

---
+++ MagicVilla.API/Data/VillaDbContext.cs
@@ -0,0 +1,90 @@
+﻿using MagicVilla.API.Models.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla.API.Data
+{
+    public class VillaDbContext:DbContext
+    {
+        public VillaDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+       
+        public DbSet<VillaNumber> Regions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var data = new List<Villa>()
+            {
+                 new Villa
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Royal Villa",
+                    Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://dotnetmastery.com/bluevillaimages/villa3.jpg",
+                    Occupancy = 4,
+                    Rate = 200,
+                    Sqft = 550,
+                    Amenity = "",
+                    CreatedDate = DateTime.Now
+                },
+              new Villa
+              {
+                  Id = Guid.NewGuid(),
+                  Name = "Premium Pool Villa",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://dotnetmastery.com/bluevillaimages/villa1.jpg",
+                  Occupancy = 4,
+                  Rate = 300,
+                  Sqft = 550,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now
+              },
+              new Villa
+              {
+                  Id = Guid.NewGuid() ,
+                  Name = "Luxury Pool Villa",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://dotnetmastery.com/bluevillaimages/villa4.jpg",
+                  Occupancy = 4,
+                  Rate = 400,
+                  Sqft = 750,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now
+              },
+              new Villa
+              {
+                  Id =Guid.NewGuid() ,
+                  Name = "Diamond Villa",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://dotnetmastery.com/bluevillaimages/villa5.jpg",
+                  Occupancy = 4,
+                  Rate = 550,
+                  Sqft = 900,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now
+              },
+              new Villa
+              {
+                  Id = Guid.NewGuid() ,
+                  Name = "Diamond Pool Villa",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://dotnetmastery.com/bluevillaimages/villa2.jpg",
+                  Occupancy = 4,
+                  Rate = 600,
+                  Sqft = 1100,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now
+              } };
+
+            modelBuilder.Entity<Villa>().HasData(data);
+            modelBuilder.Entity<VillaNumber>().HasNoKey();
+
+        }
+
+    }
+}
---
+++ MagicVilla.API/Models/Domains/VillaNumber.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla.API.Models.Domains
+{
+    public class VillaNumber
+    {
+        
+        public int VillaNo { get; set; }
+        public Guid VillaID { get; set; }
+
+        public Villa Villa { get; set; }
+
+        public string SpecialDetails { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
